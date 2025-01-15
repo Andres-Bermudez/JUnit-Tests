9eb57c3b33e60535e0276b3cfe67cca2847883f2
Andres-Bermudez <andres201900@outlook.com>
2025-01-15 18:53:10 -0500

    Adding password checker tests

---
+++ src/main/java/com/platzi/Password.java
@@ -0,0 +1,32 @@
+package com.platzi;
+
+public class Password {
+
+    public static SecurityPassword verifySecurityPassword(String password) {
+        // Verificar la longitud de la contrasena.
+        if (password == null || password.length() < 8) {
+            return SecurityPassword.WEAK;
+        }
+
+        // Expresiones regulares.
+        boolean hasLetters = password.matches(".*[a-zA-Z].*"); // Letras
+        boolean hasDigits = password.matches(".*\\d.*"); // Numeros
+        boolean hasSpecial = password.matches(".*[\\W_].*"); // Caracteres especiales.
+
+        // Evaluación de la seguridad de la contrasena.
+        // Si la contasena contiene letras, numeros y caracteres especiales.
+        if (hasLetters && hasDigits && hasSpecial) {
+            return SecurityPassword.STRONG;
+          // Si la contrasena contiene (letras y numeros) o (letras y caracteres especiales) o (numeros y caracteres especiales).
+        } else if ((hasLetters && hasDigits) || (hasLetters && hasSpecial) || (hasDigits && hasSpecial)) {
+            return SecurityPassword.MEDIUM;
+        } else {
+            return SecurityPassword.WEAK;
+        }
+    }
+
+    // Niveles de seguridad de una contrasena.
+    public enum SecurityPassword {
+        WEAK, MEDIUM, STRONG
+    }
+}
---
+++ src/test/java/com/platzi/PasswordTest.java
@@ -0,0 +1,45 @@
+package com.platzi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Tests de la seguridad de una contrasena.
+class PasswordTest {
+
+    @Test
+    @DisplayName("Menos de 8 caracteres: WEAK")
+    void verifySecurityPasswordLength() {
+        assertEquals(Password.SecurityPassword.WEAK, Password.verifySecurityPassword("abc123*"));
+    }
+
+    @Test
+    @DisplayName("Contiene letras: WEAK")
+    void verifySecurityPasswordContentLetters() {
+        assertEquals(Password.SecurityPassword.WEAK, Password.verifySecurityPassword("abcdefgh"));
+    }
+
+    @Test
+    @DisplayName("Contiene numeros: WEAK")
+    void verifySecurityPasswordContentNumbers() {
+        assertEquals(Password.SecurityPassword.WEAK, Password.verifySecurityPassword("12345678"));
+    }
+
+    @Test
+    @DisplayName("Contiene caracteres especiales: WEAK")
+    void verifySecurityPasswordContentSpecialCharacters() {
+        assertEquals(Password.SecurityPassword.WEAK, Password.verifySecurityPassword("+*&%#@!?/"));
+    }
+
+    @Test
+    @DisplayName("Contiene letras y numeros: MEDIUM")
+    void verifySecurityPasswordContentLettersAndNumbers() {
+        assertEquals(Password.SecurityPassword.MEDIUM, Password.verifySecurityPassword("abcd1234"));
+    }
+
+    @Test
+    @DisplayName("Contiene letras, numeros y caracteres especiales: STRONG")
+    void verifySecurityPasswordContentLettersNumbersAndSpecialCharacters() {
+        assertEquals(Password.SecurityPassword.STRONG, Password.verifySecurityPassword("abcd1234*+"));
+    }
+}
